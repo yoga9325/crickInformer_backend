210914f4e00465af1e7fafdc8b0460a341532c1a
yoga9325 <123081568+yoga9325@users.noreply.github.com>
2024-01-12 13:36:08 +0530

    Add files via upload

---
+++ crickbackend/CrickInformer/src/main/java/com/crick/apis/entities/Match.java
@@ -0,0 +1,174 @@
+package com.crick.apis.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cricket_match")
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int matchId;
+
+    private String teamHeading;
+    private String matchNumberVenue;
+
+    private String battingTeam;
+
+    private String battingTeamScore;
+
+    private String bowlTeam;
+    private String bowlTeamScore;
+
+    private String liveText;
+
+    private String matchLink;
+
+    private String textComplete;
+
+    @Enumerated
+    private MatchStatus status;
+
+
+  
+
+    public int getMatchId() {
+		return matchId;
+	}
+
+	@Override
+	public String toString() {
+		return "Match [matchId=" + matchId + ", teamHeading=" + teamHeading + ", matchNumberVenue=" + matchNumberVenue
+				+ ", battingTeam=" + battingTeam + ", battingTeamScore=" + battingTeamScore + ", bowlTeam=" + bowlTeam
+				+ ", bowlTeamScore=" + bowlTeamScore + ", liveText=" + liveText + ", matchLink=" + matchLink
+				+ ", textComplete=" + textComplete + ", status=" + status + ", date=" +  "]";
+	}
+
+	public void setMatchId(int matchId) {
+		this.matchId = matchId;
+	}
+
+	public String getTeamHeading() {
+		return teamHeading;
+	}
+
+	public void setTeamHeading(String teamHeading) {
+		this.teamHeading = teamHeading;
+	}
+
+	public String getMatchNumberVenue() {
+		return matchNumberVenue;
+	}
+
+	public void setMatchNumberVenue(String matchNumberVenue) {
+		this.matchNumberVenue = matchNumberVenue;
+	}
+
+	public String getBattingTeamScore() {
+		return battingTeamScore;
+	}
+
+	public void setBattingTeamScore(String battingTeamScore) {
+		this.battingTeamScore = battingTeamScore;
+	}
+
+	public String getBowlTeam() {
+		return bowlTeam;
+	}
+
+	public void setBowlTeam(String bowlTeam) {
+		this.bowlTeam = bowlTeam;
+	}
+
+	public String getBowlTeamScore() {
+		return bowlTeamScore;
+	}
+
+	public void setBowlTeamScore(String bowlTeamScore) {
+		this.bowlTeamScore = bowlTeamScore;
+	}
+
+	public String getLiveText() {
+		return liveText;
+	}
+
+	public void setLiveText(String liveText) {
+		this.liveText = liveText;
+	}
+
+	public String getMatchLink() {
+		return matchLink;
+	}
+
+	public void setMatchLink(String matchLink) {
+		this.matchLink = matchLink;
+	}
+
+	public String getTextComplete() {
+		return textComplete;
+	}
+
+	public void setTextComplete(String textComplete) {
+		this.textComplete = textComplete;
+	}
+
+	public MatchStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(MatchStatus status) {
+		this.status = status;
+	}
+
+	public Match() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Match(int matchId, String teamHeading, String matchNumberVenue, String battingTeam, String battingTeamScore,
+			String bowlTeam, String bowlTeamScore, String liveText, String matchLink, String textComplete,
+			MatchStatus status, Date date) {
+		super();
+		this.matchId = matchId;
+		this.teamHeading = teamHeading;
+		this.matchNumberVenue = matchNumberVenue;
+		this.battingTeam = battingTeam;
+		this.battingTeamScore = battingTeamScore;
+		this.bowlTeam = bowlTeam;
+		this.bowlTeamScore = bowlTeamScore;
+		this.liveText = liveText;
+		this.matchLink = matchLink;
+		this.textComplete = textComplete;
+		this.status = status;
+		
+	}
+
+	
+
+	public String getBattingTeam() {
+		return battingTeam;
+	}
+
+	public void setMatchStatus() {
+        if (this.textComplete.trim().isBlank()) {
+            this.status = MatchStatus.LIVE;
+        } else {
+            this.status = MatchStatus.COMPLETED;
+        }
+    }
+
+	public void setBattingTeam(String battingTeam2) {
+		// TODO Auto-generated method stub
+		
+	}
+}
